841c3f1a5be293a74cf6aefed9434b6e6a2c9473
gomeow <davismariotti@gmail.com>
2013-02-27 17:04:06 -0800

    Add set command

--- src/info/gomeow/points/PointEvent.java
+++ src/info/gomeow/points/PointEvent.java
@@ -1,5 +1,33 @@
 package info.gomeow.points;
 
-public class PointEvent {
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
 
+public class PointEvent extends Event {
+	
+	private static HandlerList handlers = new HandlerList();
+	private final String team;
+	private final int points;
+
+	public PointEvent(String t, int points) {
+		this.points = points;
+		this.team = t;
+	}
+	
+	public int getPoints() {
+		return points;
+	}
+	
+	public String getTeam() {
+		return this.team;
+	}
+	
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+	
 }
--- src/info/gomeow/points/Points.java
+++ src/info/gomeow/points/Points.java
@@ -2,16 +2,40 @@ package info.gomeow.points;
 
 import info.gomeow.points.util.Util;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.permissions.PermissionAttachmentInfo;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Points extends JavaPlugin {
 	
+	public boolean containsIgnoreCase(Collection<String> l, String s) {
+		 Iterator <String> it = l.iterator();
+		 while(it.hasNext()){
+			 if(it.next().equalsIgnoreCase(s)){
+				 return true;
+			 }
+		 }
+		 return false;
+	}
 	public String capFirst(String s) {
 		return Character.toUpperCase(s.charAt(0)) + s.substring(1).toLowerCase();
 	}
+	
+	public boolean permissionsStartWith(CommandSender cs, String node) {
+		for(PermissionAttachmentInfo p:cs.getEffectivePermissions()) {
+			if(p.getPermission().startsWith(node)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 
 	@Override
 	public void onEnable() {
@@ -32,7 +56,7 @@ public class Points extends JavaPlugin {
 						return true;
 					}
 					if(Util.getPoints().getConfigurationSection("Teams").getKeys(false).contains(args[1])) {
-						cs.sendMessage(ChatColor.RED+"That team has already been created!");
+						cs.sendMessage(ChatColor.RED+"That point team has already been created!");
 						return true;
 					}
 					ChatColor color = null;
@@ -52,11 +76,39 @@ public class Points extends JavaPlugin {
 						return true;
 					}
 					if(!Util.getPoints().getConfigurationSection("Teams").getKeys(false).contains(args[1])) {
-						cs.sendMessage(ChatColor.RED+"That team does not exist!");
+						cs.sendMessage(ChatColor.RED+"That point team does not exist!");
 						return true;
 					}
 				}
 				else if(args[0].equalsIgnoreCase("set")) {
+					if(this.permissionsStartWith(cs, "points.set")) {
+						if(this.containsIgnoreCase(Util.getPoints().getConfigurationSection("Teams").getKeys(false),args[1])) {
+							if(cs.hasPermission("points.set."+args[1].toLowerCase())) {
+								int points;
+								try {
+									points = Integer.parseInt(args[2]);
+								}
+								catch(NumberFormatException nfe) {
+									cs.sendMessage(ChatColor.RED+"");
+									return true;
+								}
+								Util.getPoints().set("Teams."+args[1].toLowerCase()+".Points", points);
+								Util.savePoints();
+								PointEvent e = new PointEvent(args[1].toLowerCase(),points);
+								getServer().getPluginManager().callEvent(e);
+							}
+						}
+						else {
+							cs.sendMessage(ChatColor.RED+"That point team does not exist!");
+						}
+					}
+					cs.sendMessage(ChatColor.RED+"You do not have permission to do that!");
+					return true;
+				}
+				else if(args[0].equalsIgnoreCase("give")) {
+					
+				}
+				else if(args[0].equalsIgnoreCase("take")) {
 					
 				}
 			default: sendHelp(cs);
@@ -69,6 +121,12 @@ public class Points extends JavaPlugin {
 		cs.sendMessage(ChatColor.GOLD+"/points - Shows this message.");
 		cs.sendMessage(ChatColor.GOLD+"/points create <name> - Creates a point team.");
 		cs.sendMessage(ChatColor.GOLD+"/points destroy <name> - Destroys a point team.");
+	}
+	
+	@EventHandler
+	public void onPointChange(PointEvent event) {
+		String team = event.getTeam();
+		int points = event.getPoints();
 	}
 	
 }
